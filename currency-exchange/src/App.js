2c72e75991861ff96c4b6c48f65d1df84bbdda45
Andrey Leonets <awdenheimlee@gmail.com>
2021-05-24 19:03:00 +0300

    Feat(home): create home module and add it to App

--- currency-exchange/src/App.js
+++ currency-exchange/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+import { Home } from './modules/home/Home';
 
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ currency-exchange/src/modules/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './home.scss';
+
+export const Home = () => {
+  return(
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
