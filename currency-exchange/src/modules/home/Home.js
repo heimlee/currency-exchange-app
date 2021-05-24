67814b9140aec2057e24e41a9d71fafd6d42d4b8
Andrey Leonets <awdenheimlee@gmail.com>
2021-05-25 00:25:21 +0300

    Field(home): extract components in Home module

--- currency-exchange/src/modules/home/Home.js
+++ currency-exchange/src/modules/home/Home.js
@@ -1,60 +1,16 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 
-import { Formik, Form, Field } from 'formik';
+import { FieldsCurrencyCalc } from './components/fieldsCurrencyCalc/fieldsCurrencyCalc';
+import { ConvertButton } from './components/convertButton/convertButton';
 
 import './home.scss';
 
 export const Home = () => {
-  const dispatch = useDispatch();
-
-  const initialValues = {
-    amount: '',
-    fromCurrency: '',
-    toCurrency: '',
-  };
-
-  const onSubmit = (value) => {
-    if (value.amount && value.fromCurrency && value.toCurrency) {
-      dispatch();
-    }
-  };
-
   return(
     <div className="home-container">
       <h1 className="home-title">Currency Exchange</h1>
-      <Formik 
-        initialValues={initialValues}
-        onSubmit={onSubmit}
-      >
-        <Form>
-          <div className="fields-wrapper">
-            <div className="field-container">
-              <div>Amount</div>
-              <Field type="text" id="amount" name="amount" className="field-item"></Field>
-            </div>
-            <div className="field-container">
-              <div>From</div>
-              <Field as="select" name="fromCurrency" className="field-item">
-                <option value=""></option>
-                <option value="USD">USD - US Dollar</option>
-                <option value="EUR">EUR - Euro</option>
-                <option value="RUB">RUB - Russian Ruble</option>
-                <option value="BTC">BTC - Bitcoin</option>
-              </Field>
-            </div>
-            <div className="field-container">
-              <div>To</div>
-              <Field as="select" name="toCurrency" className="field-item">
-                <option value=""></option>
-                <option value="UAH">UAH - Ukraininan Hryvnia</option>
-                <option value="USD">USD - US Dollar</option>
-              </Field>
-            </div>
-          </div>
-        </Form>
-      </Formik>
-      <button type="submit" className="convert-button">Convert</button>
+      <FieldsCurrencyCalc />
+      <ConvertButton />
     </div>
   );
 };
---
+++ currency-exchange/src/modules/home/components/fieldsCurrencyCalc/fieldsCurrencyCalc.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { Formik, Form, Field } from 'formik';
+
+import './fieldsCurrencyCalc.scss';
+
+export const FieldsCurrencyCalc = () => {
+  const dispatch = useDispatch();
+
+  const initialValues = {
+    amount: '',
+    fromCurrency: '',
+    toCurrency: '',
+  };
+
+  const onSubmit = (value) => {
+    if (value.amount && value.fromCurrency && value.toCurrency) {
+      dispatch();
+    }
+  };
+
+  return(
+    <Formik 
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+    >
+      <Form>
+        <div className="fields-wrapper">
+          <div className="field-container">
+            <div>Amount</div>
+            <Field type="text" id="amount" name="amount" className="field-item"></Field>
+          </div>
+          <div className="field-container">
+            <div>From</div>
+            <Field as="select" name="fromCurrency" className="field-item">
+              <option value=""></option>
+              <option value="USD">USD - US Dollar</option>
+              <option value="EUR">EUR - Euro</option>
+              <option value="RUB">RUB - Russian Ruble</option>
+              <option value="BTC">BTC - Bitcoin</option>
+            </Field>
+          </div>
+          <div className="field-container">
+            <div>To</div>
+            <Field as="select" name="toCurrency" className="field-item">
+              <option value=""></option>
+              <option value="UAH">UAH - Ukraininan Hryvnia</option>
+              <option value="USD">USD - US Dollar</option>
+            </Field>
+          </div>
+        </div>
+      </Form>
+    </Formik>
+  );
+};
